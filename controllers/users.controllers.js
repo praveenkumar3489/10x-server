93ff4d0ab39275ab28d8bf0f0e1db4cc92748525
praveenkumar3489 <praveen.kumar3489@gmail.com>
2020-03-05 12:25:56 +0530

    Create list api working

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -7,19 +7,37 @@ const config = require('../config');
 // test
 
 module.exports = () => {
-    return {
-        list: async(req, res) => {
-        	return await User.find();
-        },
-        getItem: async(req, res) => {
-        	let userId = req.query.id;
-        	console.log('userId >>', userId);
-        	return await User.findOne({ '_id':userId })
-        },
-        create: async(req, res) => {
-        	return new User(req.body).save()
-        },
-        update: async(req, res) => {
-        }
-    }
+	return {
+		list: async(req, res) => {
+			User.find()
+		    .exec(function(err,data){
+		        if(err) return next({
+		          error: err,
+		          code: 400
+		        })
+		        res.json({
+	                'response': data
+	            })
+		    })
+		},
+		getItem: async(req, res) => {
+			let userId = req.query.id;
+			console.log('userId >>', userId);
+			return await User.findOne({ '_id':userId })
+		},
+		create: async(req, res, next) => {
+			let newUser = new User(req.body)
+			newUser.save(function(err,data){
+				if(err) return next({
+				  error: err,
+				  code: 400
+				})
+				res.json({
+					'response': data,
+				  })
+			})
+		},
+		update: async(req, res) => {
+		}
+	}
 }
