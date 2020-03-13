c7a42b8d953e6c3c3556f4565b2f97257ad11ef2
Vinay Kumar Singh <vin05_singh@yahoo.com>
2020-03-13 17:28:23 +0530

    Timer api added

--- controllers/workshops.controllers.js
+++ controllers/workshops.controllers.js
@@ -1,87 +1,91 @@
 const Workshop = require('../models/workshops.model'),
-User = require('../models/users.model');
-let mongoose = require('mongoose'), 
-ObjectId = mongoose.Types.ObjectId;
+	User = require('../models/users.model');
+let mongoose = require('mongoose'),
+	ObjectId = mongoose.Types.ObjectId;
 
 module.exports = () => {
 	return {
-		list: async(req, res, next) => {
+		list: async (req, res, next) => {
 			let category = req.params.cid,
-			subCategory = req.params.scid.split(',');
+				subCategory = req.params.scid.split(',');
 			console.log('category:', subCategory);
-			Workshop.find({'_id':ObjectId(category), 'subCategory.text': {'$in': subCategory} })
-		    .exec(function(err,data){
-		    	console.log("err:", err);
-		        if(err) return next({
-		          error: err,
-		          code: 400
-		        })
-		        res.json({
-	                'response': data
-	            })
-		    })
-		},
-		getItem: async(req, res, next) => {
-			let workshopId = req.params.wid;
-			Workshop.findOne({ '_id':workshopId })
-			.populate('facilitators')
-			.populate('action')
-			.populate('subCategory.ideas.userId')
-		    .exec(function(err,data){
-		        if(err) return next({
-		          error: err,
-		          code: 400
-		        })
-		        res.json({
-	                'response': data
-	            })
-		    })
-		},
-		getMeta: async(req, res, next) => {
-			let workshopId = req.params.wid;
-			Workshop.findOne({ '_id':workshopId })
-			.populate('subCategory.ideas.userId')
-			.populate('facilitators')
-			.populate('team')
-		    .exec(function(err,data){
-		    	if(data.team && data.team.members) {
-		    		User.find({'_id': {'$in':data.team.members}})
-		    		.exec(function(error, members){
-		    			data.team.members = members
-		    			if(error) return next({
-				          error: error,
-				          code: 400
-				        })
-				        res.json({
-			                'response': data
-			            })
-		    		})
-				        	
-		    	} else {
-		    		if(err) return next({
-				          error: err,
-				          code: 400
-			        })
-			        res.json({
-		                'response': data
-		            })
-		    	}
-		    	
-		    })
-		},
-		create: async(req, res, next) => {
-			let newWorkshop = new Workshop(req.body)
-			newWorkshop.save(function(err,data){
-				console.log("err:", JSON.stringify(err));
-				if(err) return next({
-				  error: err,
-				  code: 400
-				})
+			Workshop.find({ _id: ObjectId(category), 'subCategory.text': { $in: subCategory } }).exec(function(
+				err,
+				data
+			) {
+				console.log('err:', err);
+				if (err)
+					return next({
+						error: err,
+						code: 400
+					});
 				res.json({
-					'response': data,
-					'code': 201
-				})
-			})
+					response: data
+				});
+			});
+		},
+		getItem: async (req, res, next) => {
+			let workshopId = req.params.wid;
+			Workshop.findOne({ _id: workshopId })
+				.populate('facilitators')
+				.populate('action')
+				.populate('subCategory.ideas.userId')
+				.exec(function(err, data) {
+					if (err)
+						return next({
+							error: err,
+							code: 400
+						});
+					res.json({
+						response: data
+					});
+				});
+		},
+		getMeta: async (req, res, next) => {
+			let workshopId = req.params.wid;
+			Workshop.findOne({ _id: workshopId })
+				.populate('subCategory.ideas.userId')
+				.populate('facilitators')
+				.populate('team')
+				.exec(function(err, data) {
+					if (data.team && data.team.members) {
+						User.find({ _id: { $in: data.team.members } }).exec(function(error, members) {
+							data.team.members = members;
+							if (error)
+								return next({
+									error: error,
+									code: 400
+								});
+							res.json({
+								response: data
+							});
+						});
+					} else {
+						if (err)
+							return next({
+								error: err,
+								code: 400
+							});
+						res.json({
+							response: data
+						});
+					}
+				});
+		},
+		create: async (req, res, next) => {
+			let newWorkshop = new Workshop(req.body);
+			newWorkshop.save(function(err, data) {
+				console.log('err:', JSON.stringify(err));
+				if (err)
+					return next({
+						error: err,
+						code: 400
+					});
+				res.json({
+					response: data,
+					code: 201
+				});
+			});
 		},
 		update: async (req, res) => {
 			Workshop.findOneAndUpdate(
@@ -96,11 +100,73 @@ module.exports = () => {
 							code: 400
 						});
 					res.json({
-						'response': "Updated Successfully",
-						'code': 200
+						response: 'Updated Successfully',
+						code: 200
 					});
 				}
 			);
+		},
+		setTimer: async (req, res) => {
+			let startTime = new Date(req.body.startTime);
+			req.body['endTime'] = startTime.getTime() + 1 * 60000;
+			Workshop.findOneAndUpdate(
+				{
+					_id: { $eq: req.params.wid }
+				},
+				req.body,
+				{
+					upsert: true
+				},
+				(err, data) => {
+					if (err)
+						return next({
+							error: err,
+							code: 400
+						});
+					res.json({
+						response: 'Data added',
+						code: 200
+					});
+				}
+			);
+		},
+		addMinute: async (req, res) => {
+			Workshop.findOne(
+				{
+					_id: { $eq: req.params.wid }
+				},
+				(err, data) => {
+					if (err)
+						return next({
+							error: err,
+							code: 400
+						});
+
+					let startTime = data.startTime ? data.startTime : Date.now();
+					let endTime = data.endTime;
+					let updatedEndTime = endTime ? endTime.getTime() + 1 * 60000 : startTime.getTime() + 1 * 60000;
+					Workshop.findOneAndUpdate(
+						{
+							_id: { $eq: req.params.wid }
+						},
+						{
+							startTime: startTime,
+							endTime: updatedEndTime
+						},
+						(err, data) => {
+							if (err)
+								return next({
+									error: err,
+									code: 400
+								});
+							res.json({
+								response: 'Updated Successfully',
+								code: 200
+							});
+						}
+					);
+				}
+			);
 		}
-	}
-}
+	};
+};
--- router/routes/workshop.routes.js
+++ router/routes/workshop.routes.js
@@ -1,20 +1,17 @@
 'use strict';
 
-module.exports = (app) => {
-    var router = require('express').Router();
-    
-    var controller = require('../../controllers/workshops.controllers')();
-    // const middlewares = require('../../middlewares');
+module.exports = app => {
+	var router = require('express').Router();
 
-    router.route('/allWorkshop/:cid/:scid')
-        .get(controller.list);
-    router.route('/workshop/:wid/')
-        .get(controller.getItem);
-    router.route('/workshop/:wid/meta')
-        .get(controller.getMeta);
-    router.route('/workshop')
-        .post(controller.create);
-    router.route('/workshop/:wid')
-        .put(controller.update);
-    app.use('/api',router);
+	var controller = require('../../controllers/workshops.controllers')();
+	// const middlewares = require('../../middlewares');
+
+	router.route('/allWorkshop/:cid/:scid').get(controller.list);
+	router.route('/workshop/:wid/').get(controller.getItem);
+	router.route('/workshop/:wid/meta').get(controller.getMeta);
+	router.route('/workshop').post(controller.create);
+	router.route('/workshop/:wid').put(controller.update);
+	router.route('/workshop/:wid/startTimer').post(controller.setTimer);
+	router.route('/workshop/:wid/addMinute').post(controller.addMinute);
+	app.use('/api', router);
 };
